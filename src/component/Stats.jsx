825b8155cf83ec8d785138cac25dea195b2792c7
Rajeshsharma-123 <141806955+Rajeshsharma-123@users.noreply.github.com>
2024-11-14 12:28:19 +0545

    about section updated

--- src/component/Stats.jsx
+++ src/component/Stats.jsx
@@ -9,7 +9,7 @@ const Stats = () => {
     {stats.map(({ no, title }, index) => {
         return (
             <div className="stats__box" key={index}>
-                <h3 className="stats__no">{no}</h3>
+                <h3 className="stats__no">{parse(no)}</h3>
                 <p className="stats__title">{parse(title)}</p>
             </div>
         )
